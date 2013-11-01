1772f0d03661917d97c66b7a21be42667035b4a4
Oscar Fonts <oscar.fonts@gmail.com>
2013-11-01 10:35:41 +0100

    Display cultures & clusters as bootstrap buttons

--- web/static/js/modules/map.navigator.js
+++ web/static/js/modules/map.navigator.js
@@ -1,44 +1,29 @@
 define(["eventbus", "leaflet", "map", "data.feature", "markercluster"], function(events, L, map, features) {
    
-   features.list("peca");
+    features.list("cultura");
    
-   events.listen("data.feature.listed", function(event, data) {
+    events.listen("data.feature.listed", function(event, data) {
         var clustered = L.markerClusterGroup({
             showCoverageOnHover: false,
-            maxClusterRadius: 80,
-            spiderfyDistanceMultiplier: 2,
+            maxClusterRadius: 75,
+            spiderfyDistanceMultiplier: 3,
             iconCreateFunction: function (cluster) {
-                /*
-                var num = Math.min(cluster.getChildCount(), 32);
-                var size = 21 + num;
-                var hue = 120 - (3.75 * num);
-                return L.divIcon({
-                    className: 'qwer',
-                    iconSize: [size, size],
-                    html: "<div class='mcm-marker-cluster' style='width:"+size+"px;height:"+size+"px;color:hsl("+hue+", 100%, 40%);background-color:hsl("+hue+", 50%, 80%)'>"+cluster.getChildCount()+"</div>"                    
-                });
-                */
                return L.divIcon({
-                    className: 'mcm-marker-cluster',
-                    iconSize: [23, 23],
-                    html: cluster.getChildCount()
+                    className: 'mcm-hide-marker',
+                    html: '<div class="btn btn-xs btn-danger"><b>'+cluster.getChildCount()+'</b> cultures</div>'
                 });
             },
         });
         
         L.geoJson(data.features, {
             onEachFeature: function(feature, layer) {
-                if(layer instanceof L.Marker) {
-                    layer.setIcon(L.icon({
-                        iconUrl: 'static/img/peces/'+feature.id+'.JPG',
-                        //shadowUrl: 'leaf-shadow.png',
-                        className: "mcm-marker-icon",
-                        iconSize:     [64, 64]//, // size of the icon
-                        //shadowSize:   [50, 64], // size of the shadow
-                        //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                        //shadowAnchor: [4, 62],  // the same for the shadow
-                        //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-                    }));
+                if (layer instanceof L.Marker) {
+                    layer.setIcon(
+                        L.divIcon({
+                            className: 'mcm-hide-marker',
+                            html: '<div class="btn btn-xs btn-primary">'+feature.properties.nom+'</div>'
+                        })
+                    );
                 }
                 clustered.addLayer(layer);
             }
@@ -46,5 +31,5 @@ define(["eventbus", "leaflet", "map", "data.feature", "markercluster"], function
         
         map.addLayer(clustered);
         map.fitBounds(clustered.getBounds());
-   });
+    });
 });
