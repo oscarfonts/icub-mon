f29813a83e654a0a76abfaffe985336474168cab
Oscar Fonts <oscar.fonts@gmail.com>
2013-11-12 16:46:00 +0100

    Added culture editor (GUI). #5

--- web/admin/static/admin.js
+++ web/admin/static/admin.js
@@ -3,13 +3,16 @@ var LIB_PATH = "../lib/";
 require.config({
     baseUrl : "../static/js/modules/",
     paths : {
-        "jquery":        LIB_PATH + "jquery",
-        "bootstrap":     LIB_PATH + "bootstrap",
-        "leaflet":       LIB_PATH + "leaflet-src",
-        "bing":          LIB_PATH + "Bing",
-        "draw":          LIB_PATH + "leaflet.draw",
+        "jquery": LIB_PATH + "jquery",
+        "bootstrap": LIB_PATH + "bootstrap",
+        "leaflet": LIB_PATH + "leaflet-src",
+        "bing": LIB_PATH + "Bing",
+        "draw": LIB_PATH + "leaflet.draw",
         "ie8.html5shiv": LIB_PATH + "html5shiv",
-        "ie8.respond":   LIB_PATH + "respond.min"
+        "ie8.respond": LIB_PATH + "respond.min",
+        "wysihtml5": LIB_PATH + "bootstrap3-wysihtml5",
+        "wysihtml5-base": LIB_PATH + "wysihtml5-0.3.0",
+        "wysihtml5-ca": LIB_PATH + "bootstrap-wysihtml5.ca-CT"
     },
     shim: {
         "bootstrap": {
@@ -23,6 +26,12 @@ require.config({
         },
         "L.Control.DrawSingle": {
             deps: ["draw"]
+        },
+        "wysihtml5": {
+            deps: ["bootstrap","wysihtml5-base"]
+        },
+        "wysihtml5-ca": {
+            deps: ["wysihtml5"]
         }
     },
     config: {
--- web/static/js/modules/admin.details.js
+++ web/static/js/modules/admin.details.js
@@ -1,4 +1,4 @@
-define(["eventbus", "jquery", "tree"], function(events) {
+define(["eventbus", "jquery", "tree", "wysihtml5-ca"], function(events) {
 
     events.listen("tree.continentSelected", renderContinent);  
     events.listen("tree.culturaSelected", renderCultura);
@@ -14,14 +14,48 @@ define(["eventbus", "jquery", "tree"], function(events) {
     
     function renderCultura(event, cultura) {
         title("Cultura " + cultura.nom + " (" + cultura.id + ")");
-        details("");
-        tagline("Dibuixeu l'àmbit geogràfic de la cultura, o sel·leccioneu una peça de la llista");
+        details('<div class="well well-lg"><textarea class="form-control framed" id="editor-cultura" placeholder="Afegiu una descripció per a la cultura ' + cultura.nom + '..." style="width: 100%; height: 400px"></textarea></div>');
+        tagline("Editeu la cultura, o sel·leccioneu una peça de la llista");
+        
+        // Opcions de l'editor
+        $('#editor-cultura').wysihtml5({
+            "font-styles": false, //Font styling, e.g. h1, h2, etc. Default true
+            "emphasis": true, //Italics, bold, etc. Default true
+            "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+            "html": false, //Button which allows you to edit the generated HTML. Default false
+            "link": false, //Button to insert a link. Default true
+            "image": false, //Button to insert an image. Default true,
+            "color": false, //Button to change color of font
+            "size": 'sm', //Button size like sm, xs etc.
+            "locale": "ca-CT"
+            /* EVENTS! https://github.com/xing/wysihtml5/wiki/Events#list-of-supported-events
+            "events": {
+                "load": function() { 
+                    console.log("Loaded!");
+                },
+                "blur": function() { 
+                    console.log("Blured");
+                }
+            }
+            */
+        });
+        
+        $('.wysihtml5-toolbar').prepend('<li class="well-title">Descripció de ' + cultura.nom + '</li>');
+        
+        $('.wysihtml5-toolbar').append($('<li style="float:right;">')
+            .append($('<div class="btn-group">')
+                .append('<a class="btn btn-success btn-sm" tabindex="-1" title="Desa" href="javascript:;"><i class="glyphicon glyphicon-ok"></i> Desa</a>')
+                .append('<a class="btn btn-danger btn-sm" tabindex="-1" title="Descarta" href="javascript:;"><i class="glyphicon glyphicon-remove"></i> Descarta</a>')
+            )
+        );
+        
+        //$('#editor-cultura').val(); // Retrieve value
     };
     
     function renderPeca(event, peca) {
        
         var html = 
-             '<div><table><tr><td><img src="../static/img/peces/'+peca.id+'.JPG" alt="'+peca.num_registre+'"></td>' +
+             '<div class="framed"><table><tr><td><img src="../static/img/peces/'+peca.id+'.JPG" alt="'+peca.num_registre+'"></td>' +
              '<td><ul><li><b>Procedència:</b> '+peca.procedencia+'</li>';
         if (peca.precisions_procedencia.length) {
              html +='<li><b>Precisions procedència:</b> '+peca.precisions_procedencia+'</li>';
