9d0a2c4209559fc5f6f4d153ef230aa5f14fc4b3
Oscar Fonts <oscar.fonts@gmail.com>
2014-01-15 10:38:10 +0100

    template: async rendering, returning promise

--- web/modules/template.js
+++ web/modules/template.js
@@ -1,16 +1,21 @@
 /**
  * @author Oscar Fonts <oscar.fonts@geomati.co>
  */
-define(["jquery", "mustache"], function($, Mustache) {
+
+define(["mustache", "jquery"], function(Mustache, $) {
     
     return {
         render: function(template_name, data, target_id) {
+            var defer = $.Deferred();
+
             var import_tag = "text!../templates/"+template_name+".template.html";
             require([import_tag], function(template) {
                 var result = Mustache.render(template, data);
-                $("#"+target_id).html(result);
+                document.getElementById(target_id).innerHTML = result;
+                defer.resolve();
             });
 
+            return defer.promise();
         }
     };
 });
