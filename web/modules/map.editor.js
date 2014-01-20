49e8555bea2ca6911adc056e636a380bcb30fa0d
Oscar Fonts <oscar.fonts@gmail.com>
2014-01-20 14:23:37 +0100

    Admin: Feature Editor

--- web/modules/map.editor.js
+++ web/modules/map.editor.js
@@ -1,11 +1,13 @@
-define(["messagebus", "leaflet", "map", "draw", "L.Control.DrawSingle" /*, "data.feature"*/], function(bus, L, map) {
-    var activeGeom = {};
-
-    var drawItems = L.featureGroup().addTo(map);
-
-    var drawControl = new L.Control.DrawSingle({
+define(["messagebus", "leaflet", "map", "draw", "L.Control.DrawSingle"], function(bus, L, map) {
+    
+    // Initialize editor
+    var loaded = {};
+    
+    var leafletItems = L.featureGroup().addTo(map);
+    
+    var leafletDraw = new L.Control.DrawSingle({
         edit: {
-            featureGroup: drawItems
+            featureGroup: leafletItems
         },
         draw: {
             polyline: false,
@@ -14,18 +16,18 @@ define(["messagebus", "leaflet", "map", "draw", "L.Control.DrawSingle" /*, "data
         }
     }).addTo(map);
     
-    bus.subscribe("data.feature.none", function(event) {
-        drawItems.clearLayers();
-        activeGeom = {};
-        drawControl.hide();
-    }, this);
-           
-    bus.subscribe("data.feature.read", function(event, data) {
-        drawItems.clearLayers();
-        activeGeom = data;
-        var feature = L.geoJson(data.feature, {
+    function load(type, id, feature) {
+        leafletItems.clearLayers();
+
+        loaded = {
+            type: type,
+            id: id,
+            feature: feature
+        };
+
+        L.geoJson(feature, {
             onEachFeature: function (feature, layer) {
-                drawItems.addLayer(layer);
+                leafletItems.addLayer(layer);
                 if (layer instanceof L.Marker) {
                     map.setView(layer.getLatLng(), 6, {animate: true});
                 } else {
@@ -33,27 +35,35 @@ define(["messagebus", "leaflet", "map", "draw", "L.Control.DrawSingle" /*, "data
                 }
             }
         });
-     }, this);
-   
-    bus.subscribe("data.feature.notFound", function(event, data) {
-        drawItems.clearLayers();
-        activeGeom = data;
-        drawControl.show();
-    }, this);
+        
+        leafletDraw.show();
+    }
+    
+    function hide() {
+        leafletItems.clearLayers();
+        loaded = {};
+        leafletDraw.hide();
+    }
     
     map.on('draw:created', function (e) {
         var feature = e.layer.toGeoJSON();
-        feature.id = activeGeom.id;
-        bus.publish("map.editor.featureCreated", {type: activeGeom.type, id: activeGeom.id, feature: feature});
+        feature.id = loaded.id;
+        bus.publish("map.editor.featureCreated", {type: loaded.type, id: loaded.id, feature: feature});
     });
     
     map.on('draw:edited', function (e) {
         var feature = e.layers.getLayers()[0].toGeoJSON();
-        feature.id = activeGeom.id;
-        bus.publish("map.editor.featureEdited", {type: activeGeom.type, id: activeGeom.id, feature: feature});
+        feature.id = loaded.id;
+        bus.publish("map.editor.featureEdited", {type: loaded.type, id: loaded.id, feature: feature});
     });
     
     map.on('draw:deleted', function (e) {
-        bus.publish("map.editor.featureDeleted", {type: activeGeom.type, id: activeGeom.id});
-    });    
+        bus.publish("map.editor.featureDeleted", {type: loaded.type, id: loaded.id});
+    });
+    
+    return {
+        load: load,
+        hide: hide
+    };
+    
 });
--- web/modules/mcm.admin.js
+++ web/modules/mcm.admin.js
@@ -1,21 +1,68 @@
-define(["messagebus", "login", "mcm.tree", "mcm.object", "mcm.description.editor", "map.editor"], function(bus, login, tree, object) {
+define(["messagebus", "login", "mcm.api", "map.editor", "mcm.tree", "mcm.object", "mcm.description.editor"],
+    function(bus, login, mcm, mapeditor, tree, object) {
 
+    // Setup login module
     login.linkTo("login", "entrar", "sortir");
-    tree.setDiv("tree");
-    object.setDiv("details");
-
     login.onLogin(show);
     login.onLogout(hide);   
-    
     // TODO: Remove this
     login.forceLogin("test", "test");
 
+    // Set tree and object divs
+    tree.setDiv("tree");
+    object.setDiv("details");
+
+    // Handle tree element selection
     bus.subscribe("mcm.tree.item_selected", function(selected) {
-        if (selected.type == "object") {
+        var type = selected.type;
+        var id = selected.item.id;
+        
+        // Set map editor
+        if (type == "collection") { // A continent => no map editor
+            mapeditor.hide();
+        } else {
+            mcm[type].get(id).then(
+                function(feature) {
+                    mapeditor.load(type, id, feature);
+                },
+                function(error) {
+                    mapeditor.load(type, id);
+                }
+            );
+        }
+        
+        // Set object details
+        if (type == "object") {
             object.show(selected.item.collection, selected.item.id);
         } else {
             object.hide();
         }
+    });
+    
+    bus.subscribe("map.editor.featureCreated", function(data) {
+        // Add alphanumeric feature properties
+        var item = tree.getItem(data.feature.id);
+        if (item.hasOwnProperty("culture")) {
+            data.feature.properties["culture"] = item.culture;
+        } else {
+            data.feature.properties["continent"] = item.collection;
+        }
+        
+        mcm[data.type].create(data.feature).then(function(feature) {
+            mapeditor.load(data.type, feature.id, feature);
+        });
+    });
+
+    bus.subscribe("map.editor.featureEdited", function(data) {
+        mcm[data.type].update(data.feature).then(function(feature) {
+            mapeditor.load(data.type, feature.id, feature);
+        });
+    });
+
+    bus.subscribe("map.editor.featureDeleted", function(data) {
+        mcm[data.type].del(data.id).then(function() {
+            mapeditor.load(data.type, data.id);
+        });
     });
     
     function show(username) {
