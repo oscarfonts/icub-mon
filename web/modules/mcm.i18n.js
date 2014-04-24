9190d9592d982b39cb28bb918a9c5b2044655a6b
Micho Garcia <michogar@gmail.com>
2014-04-24 14:22:05 +0200

    Setting català as default language when no language in URL #16

--- web/modules/mcm.i18n.js
+++ web/modules/mcm.i18n.js
@@ -14,6 +14,7 @@ define(['jquery', 'jquery-i18n'], function($) {
 
 		init : function() {
 			var language = replaceAll(window.location.pathname, '/', '');
+			language = (language == '') ? 'ca' : language;
 			this.setLang(language);
 		},
 
