170a0883dc8ba9b0d141dbe533607b406b0bcaf3
Micho Garcia <michogar@gmail.com>
2014-04-29 12:54:48 +0200

    Added translate method to template.js
    Translated mcm.search.js

--- web/modules/template.js
+++ web/modules/template.js
@@ -1,13 +1,18 @@
 /**
  * @author Oscar Fonts <oscar.fonts@geomati.co>
  */
-define(["mustache", "jquery"], function(Mustache, $) {
+define(["mcm.i18n", "mustache", "jquery"], function(i18n, Mustache, $) {
     
     return {
         render: function(template_name, data, target_id) {
             var defer = $.Deferred();
 
             var import_tag = "text!../templates/"+template_name+".html";
+            data.translate = function() {
+            	return function(text) {
+            		return i18n.translate(text);
+            	};
+            };
             require([import_tag], function(template) {
                 var result = Mustache.render(template, data);
                 document.getElementById(target_id).innerHTML = result;
